b89ba6c3c6f996bb940f5a88e1f87707b28dfde3
Macronut <Macronut@live.com>
2018-06-05 14:52:11 +0800

    Update sni_cover.py

--- sni_cover.py
+++ sni_cover.py
@@ -10,19 +10,21 @@ INTERFACE = ('0.0.0.0', 443)
 WAN = ''
 
 HOSTS = {
-    'api.twitter.com'    : (10, 0, ['104.244.45.247']),
-    'twitter.com'        : (10, 0, ['104.244.45.254']),
-    'mobile.twitter.com' : (10, 0, ['104.244.45.247', '104.244.45.255']),
-    'abs.twimg.com'      : (10, 0, ['104.244.43.98', '104.244.46.135', '104.244.43.130', '104.244.43.66']),
-    'pbs.twimg.com'      : (10, 0, ['104.244.43.98', '104.244.46.135', '104.244.43.2', '104.244.43.66']),
-    'video.twimg.com'    : (10, 0, ['104.244.43.98', '104.244.43.2', '104.244.43.66', '104.244.43.106']),
-    'abs-0.twimg.com'    : (10, 0, ['104.244.43.98', '104.244.46.135', '104.244.43.130', '104.244.43.2']),
-    'www.instagram.com'                : (10, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
-    'i.instagram.com'                  : (10, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
-    'graph.instagram.com'              : (10, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
-    'scontent-sit4-1.cdninstagram.com' : (0, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
-    'scontent-lax3-2.cdninstagram.com' : (0, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
-    'scontent-ams3-1.cdninstagram.com' : (0, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
+    'api.twitter.com'    : (True, 0.1, 10, 0, ['104.244.45.247']),
+    'twitter.com'        : (True, 0.1, 10, 0, ['104.244.45.254']),
+    'mobile.twitter.com' : (True, 0.1, 10, 0, ['104.244.45.247', '104.244.45.255']),
+    'abs.twimg.com'      : (True, 0.1, 10, 0, ['104.244.43.98', '104.244.46.135', '104.244.43.130', '104.244.43.66']),
+    'pbs.twimg.com'      : (True, 0.1, 10, 0, ['104.244.43.98', '104.244.46.135', '104.244.43.2', '104.244.43.66']),
+    'video.twimg.com'    : (True, 0.1, 10, 0, ['104.244.43.98', '104.244.43.2', '104.244.43.66', '104.244.43.106']),
+    'abs-0.twimg.com'    : (True, 0.1, 10, 0, ['104.244.43.98', '104.244.46.135', '104.244.43.130', '104.244.43.2']),
+    'www.instagram.com'                : (True, 0.1, 10, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
+    'i.instagram.com'                  : (True, 0.1, 10, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
+    'graph.instagram.com'              : (True, 0.1, 10, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
+    'scontent-sit4-1.cdninstagram.com' : (True, 0.1, 0, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
+    'scontent-lax3-2.cdninstagram.com' : (True, 0.1, 0, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
+    'scontent-ams3-1.cdninstagram.com' : (True, 0.1, 0, 0, ['31.13.75.174', '31.13.73.172', '31.13.73.174', '31.13.72.172',]),
+    'SNI'                : (True, 1, 12, 0, ['213.184.119.13', '213.184.119.116', '213.184.119.30', '213.184.119.97', '213.184.119.110', '213.184.119.118', '213.184.119.2', '213.184.119.44', '213.184.119.36', '213.184.119.46', '213.184.119.117', '213.184.119.120', '213.184.119.80', '213.184.119.9', '213.184.119.86', '213.184.119.57', '213.184.119.99', '213.184.119.32', '213.184.119.10', '213.184.119.70', '213.184.119.75', '213.184.119.84', '213.184.119.49', '213.184.119.124', '213.184.119.61', '213.184.119.62', '213.184.119.98', '213.184.119.79', '213.184.119.60', '213.184.119.15', '213.184.119.14', '213.184.119.122', '213.184.119.76', '213.184.119.39', '213.184.119.90', '213.184.119.65', '213.184.119.7', '213.184.119.50', '213.184.119.54', '213.184.119.77', '213.184.119.82', '213.184.119.64', '213.184.119.28', '213.184.119.51', '213.184.119.114', '213.184.119.20', '213.184.119.112', '213.184.119.6', '213.184.119.68', '213.184.119.113', '213.184.119.66', '213.184.119.21', '213.184.119.53', '213.184.119.17', '213.184.119.34', '213.184.119.67', '213.184.119.83', '213.184.119.8', '213.184.119.93', '213.184.119.18', '213.184.119.94', '213.184.119.100', '213.184.119.102', '213.184.119.45', '213.184.119.96', '213.184.119.56', '213.184.119.29', '213.184.119.89', '213.184.119.108', '213.184.119.58', '213.184.119.87', '213.184.119.119', '213.184.119.125', '213.184.119.35', '213.184.119.123', '213.184.119.71', '213.184.119.121', '213.184.119.27', '213.184.119.23', '213.184.119.107', '213.184.119.48', '213.184.119.16', '213.184.119.52', '213.184.119.73', '213.184.119.31', '213.184.119.24', '213.184.119.78', '213.184.119.105', '213.184.119.85', '213.184.119.92', '213.184.119.103', '213.184.119.111', '213.184.119.74', '213.184.119.25', '213.184.119.41', '213.184.119.115', '213.184.119.88', '213.184.119.126', '213.184.119.43', '213.184.119.5', '213.184.119.59', '213.184.119.38', '213.184.119.101', '213.184.119.63', '213.184.119.3', '213.184.119.47', '213.184.119.81', '213.184.119.69', '213.184.119.55', '213.184.119.26', '213.184.119.19', '213.184.119.11', '213.184.119.4', '213.184.119.95', '213.184.119.22', '213.184.119.40', '213.184.119.106', '213.184.119.33', '213.184.119.12', '213.184.119.91', '213.184.119.42', ]),
+    '.google.com'        : 'SNI',
     }
 
 class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
@@ -108,24 +110,10 @@ class SNIProxy(SocketServer.StreamRequestHandler):
         try:
             remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             #remote.setsockopt(socket.SOL_TCP, socket.TCP_MAXSEG, 384)
-            remote.settimeout(1.5)
+            remote.settimeout(2.0)
             
             if ttl > 0:
-                port = 0
-                while not self.ready:
-                    try:
-                        port = random.randint(32768, 65535)
-                        remote.bind((WAN, port))
-                        break
-                    except socket.error, e:
-                        print e
-                
-                if port == 0:
-                    remote.close()
-                    return
-                
                 s_recv_tcp = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-                s_recv_tcp.bind((WAN, port))
                 s_recv_tcp.connect(addr)
                 remote.connect(addr)
                 
@@ -140,43 +128,41 @@ class SNIProxy(SocketServer.StreamRequestHandler):
                 
                 sockname = remote.getsockname()
                 fakedata = ' ' * len(data)
-                    
-                packet = s_recv_tcp.recv(2048)
-                
-                tcp_header = packet[20:40]
-                tcph = struct.unpack(b'!HHIIBBHHH', tcp_header)
-                sport, dport, seq, aseq, headlen, flags, win, chechsum, p = tcph
-                
-                if dport != sockname[1]:
-                    print 'Bind Error'
-                    print remote.getpeername(), sockname, dport, sport, port
-                    s_recv_tcp.close()
-                    remote.close()
-                    return
 
-                if self.ready:
-                    s_recv_tcp.close()
-                    remote.close()
-                    return
-                
-                if ttl == 1:
-                    tcp_header = struct.pack(b'!HHLLBBHHH', sockname[1], addr[1], aseq, seq, 80, 24, 454, 0, 0)
-                    packet = tcp_header + fakedata
-                    s_recv_tcp.sendto(packet, (addr[0], 0))
-                    s_recv_tcp.sendto(packet, (addr[0], 0))
-                else:
-                    s_send = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-                    iph = struct.unpack(b'!BBHHHBBH4s4s', packet[0:20])
-                    ip_header = struct.pack('!BBHHHBBH4s4s' , 69, 0, 0, 47843, 16384, ttl, 6, 0, iph[9], iph[8])
-                    tcp_header = struct.pack(b'!HHLLBBHHH', sockname[1], addr[1], aseq, seq+1, 80, 24, 454, 0, 0)
-                    tcp_packet = tcp_header + fakedata
-                    psh = struct.pack(b'!4s4sBBH', iph[9], iph[8], 0, socket.IPPROTO_TCP, len(tcp_packet))
-                    tcp_checksum = checksum(psh + tcp_packet)
-                    tcp_header = struct.pack(b'!HHLLBBHHH', sockname[1], addr[1], aseq, seq+1, 80, 24, 454, tcp_checksum, 0)
-                    packet = ip_header + tcp_header + fakedata
-                    s_send.sendto(packet, (addr[0], 0))
-                    s_send.sendto(packet, (addr[0], 0))
-                    s_send.close()
+                while not self.ready:
+                    packet = s_recv_tcp.recv(2048)
+                    
+                    tcp_header = packet[20:40]
+                    tcph = struct.unpack(b'!HHIIBBHHH', tcp_header)
+                    sport, dport, seq, aseq, headlen, flags, win, chechsum, p = tcph
+                    
+                    if dport != sockname[1]:
+                        continue
+
+                    if self.ready:
+                        s_recv_tcp.close()
+                        remote.close()
+                        return
+                    
+                    if ttl == 1:
+                        tcp_header = struct.pack(b'!HHLLBBHHH', sockname[1], addr[1], aseq, seq, 80, 24, 454, 0, 0)
+                        packet = tcp_header + fakedata
+                        s_recv_tcp.sendto(packet, (addr[0], 0))
+                        s_recv_tcp.sendto(packet, (addr[0], 0))
+                    else:
+                        s_send = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+                        iph = struct.unpack(b'!BBHHHBBH4s4s', packet[0:20])
+                        ip_header = struct.pack('!BBHHHBBH4s4s' , 69, 0, 0, 47843, 16384, ttl, 6, 0, iph[9], iph[8])
+                        tcp_header = struct.pack(b'!HHLLBBHHH', sockname[1], addr[1], aseq, seq+1, 80, 24, 454, 0, 0)
+                        tcp_packet = tcp_header + fakedata
+                        psh = struct.pack(b'!4s4sBBH', iph[9], iph[8], 0, socket.IPPROTO_TCP, len(tcp_packet))
+                        tcp_checksum = checksum(psh + tcp_packet)
+                        tcp_header = struct.pack(b'!HHLLBBHHH', sockname[1], addr[1], aseq, seq+1, 80, 24, 454, tcp_checksum, 0)
+                        packet = ip_header + tcp_header + fakedata
+                        s_send.sendto(packet, (addr[0], 0))
+                        s_send.sendto(packet, (addr[0], 0))
+                        s_send.close()
+                    break
             
                 s_recv_tcp.close()
                 
@@ -220,9 +206,22 @@ class SNIProxy(SocketServer.StreamRequestHandler):
             event_ready = threading.Event()
 
             threadlist = []
+
             if HOSTS.has_key(server_name):
-                ttl, mss, addrlist = HOSTS[server_name]
-                random.shuffle(addrlist)
+                rule = HOSTS[server_name]
+            else:
+                root_name = server_name[server_name.find('.'):]
+                if HOSTS.has_key(root_name):
+                    rule = HOSTS[root_name]
+                else:
+                    rule = None
+            
+            if rule:
+                if isinstance(rule, str):
+                    rule = HOSTS[rule]
+                rand, waittime, ttl, mss, addrlist = rule
+                if rand:
+                    random.shuffle(addrlist)
                 count = len(addrlist)
                 for i in xrange(count):
                     addr = (addrlist[i], port)
@@ -230,10 +229,14 @@ class SNIProxy(SocketServer.StreamRequestHandler):
                     c.start()
                     threadlist.append(c)
                     if i < count-1:
-                        if event_connected.wait(timeout=0.1):
+                        if event_connected.wait(timeout=waittime):
                             break
                     else:
                         event_connected.wait()
+            else:
+                logging.info('%s->%s %s' % (self.client_address[0], server_name, 'Unknow'))
+                sock.close()
+                return
             
             event_ready.wait()
             
